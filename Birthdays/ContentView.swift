21352414a3f5e32492a861cadbd52dd6b927d409
Jeremy Kim <jedo@alumni.unc.edu>
2025-05-09 23:23:27 -0400

    Initial Commit

---
+++ Birthdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birthdays
+//
+//  Created by Jeremy Kim on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
